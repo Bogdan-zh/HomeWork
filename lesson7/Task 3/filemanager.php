66d0b42869255130c541bf91ee715a873a0ab8ad
Bogdan-zh <32609319+Bogdan-zh@users.noreply.github.com>
2017-10-26 15:36:56 +0300

    hw7

--- lesson7/Task 3/filemanager.php
+++ lesson7/Task 3/filemanager.php
@@ -14,82 +14,82 @@ function translit($str) {
 
 
 function uploadImage($directory) {
-	if(!is_dir($directory)) {
-		mkdir($directory);
- 	}
-  	if($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_FILES)) {
-  		
-  		$array_ext = array('jpg','jpeg','png','gif');
-  		$files = $_FILES['files'];
-  		$cnt = count($files['name']);
+    if(!is_dir($directory)) {
+        mkdir($directory);
+    }
+    if($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_FILES)) {
+        
+        $array_ext = array('jpg','jpeg','png','gif');
+        $files = $_FILES['files'];
+        $cnt = count($files['name']);
 
-  		for($i=0; $i < $cnt; $i++) {
-  			$name = pathinfo($files['name'][$i], PATHINFO_FILENAME);
-  			$ext = pathinfo($files['name'][$i], PATHINFO_EXTENSION);
-  			if(in_array($ext, $array_ext)) {
-  				$hash = substr(md5($name.date('Y-m-d-h-i-s').rand(1,1000)),0,10);
-  				$filename = translit($name)."_".$hash.".".$ext;
-  				if(move_uploaded_file($files['tmp_name'][$i], $directory.$filename)) {
-  					echo "Файл <span class=\"fileok\">$name.$ext</span> загружен в папку $directory <br>";
-  				} else {
-  					echo 'ERROR';
-  				}
+        for($i=0; $i < $cnt; $i++) {
+            $name = pathinfo($files['name'][$i], PATHINFO_FILENAME);
+            $ext = pathinfo($files['name'][$i], PATHINFO_EXTENSION);
+            if(in_array($ext, $array_ext)) {
+                $hash = substr(md5($name.date('Y-m-d-h-i-s').rand(1,1000)),0,10);
+                $filename = translit($name)."_".$hash.".".$ext;
+                if(move_uploaded_file($files['tmp_name'][$i], $directory.$filename)) {
+                    echo "Файл <span class=\"fileok\">$name.$ext</span> загружен в папку $directory <br>";
+                } else {
+                    echo 'ERROR';
+                }
 
-  			} elseif(($_FILES['files']['name']['0']) == '') {
-  				echo "Вы ничего не загрузили, выберите файлы для загрузки, и нажмите кнопку 'Отправить'.";
-  			} else {
-  				echo "Файл <span class=\"fileerror\">$name.$ext</span> не загружен, недопустимый формат файла.";
-  			}
-  		}
-  	}
+            } elseif(($_FILES['files']['name']['0']) == '') {
+                echo "Вы ничего не загрузили, выберите файлы для загрузки, и нажмите кнопку 'Отправить'.";
+            } else {
+                echo "Файл <span class=\"fileerror\">$name.$ext</span> не загружен, недопустимый формат файла.";
+            }
+        }
+    }
 }
 
  ?>
 
 <div class="left">
-	<div class="container">
-		<h2>Загрузка фото</h2>
-		<form method="post" enctype="multipart/form-data">
-			<input type="file" name="files[]" multiple>
-			<input type="submit">
-		</form>
-		<?php 
-			uploadImage("files/");
-		?>
-	</div>
+    <div class="container">
+        <h2>Загрузка фото</h2>
+        <form method="post" enctype="multipart/form-data">
+            <input type="file" name="files[]" multiple>
+            <input type="submit">
+        </form>
+        <?php 
+            uploadImage("files/");
+        ?>
+    </div>
 </div>
 <div class="right">
-	<div class="container">
-		<h2>Содержимое папки files</h2>
-		<?php
+    <div class="container">
+        <h2>Содержимое папки files</h2>
+        <?php
 
-		$scandir = scandir("files/"); 
-		//unset($scandir[0], $scandir[1]);
-		array_splice($scandir, 0, 2);
+        $scandir = scandir("files/"); 
+        //unset($scandir[0], $scandir[1]);
+        array_splice($scandir, 0, 2);
 
-		// Раскоментировать, если нужно посмотреть названия картинок
+        // Раскоментировать, если нужно посмотреть названия картинок
 
-		// echo "<div class='names'>";
-		// if (!empty(scandir("files/"))) {
-		// 	echo "<pre>";
+        // echo "<div class='names'>";
+        // if (!empty(scandir("files/"))) {
+        //  echo "<pre>";
 
-		// 	array_splice($scandir, 0, 0);
-		// 	print_r($scandir);
+        //  array_splice($scandir, 0, 0);
+        //  print_r($scandir);
 
-		// 	//echo implode("<br>", $scandir); // это раскоментировать, если нужны названия НЕ в виде массива
+        //  //echo implode("<br>", $scandir); // это раскоментировать, если нужны названия НЕ в виде массива
 
-		// 	echo "</pre>";
-		// }
-		// echo "</div>";
-		
-		?>
-		
-		<?php foreach($scandir as $img): ?>
-			<a href="files/<?php echo $img ?>"> 
-				<img src="files/<?php echo $img ?>"  alt="">
-			</a>
-		<?php endforeach; ?>
+        //  echo "</pre>";
+        // }
+        // echo "</div>";
+        
+        ?>
+        
+        <?php foreach($scandir as $img): ?>
+            <a href="files/<?php echo $img ?>"> 
+                <img src="files/<?php echo $img ?>"  alt="">
+            </a>
+        <?php endforeach; ?>
 
-	</div>
+    </div>
 </div>
 <div style="clear: both;"></div>
