6c57a96d157a8e24040270fe5dd970009943a321
Bogdan-zh <32609319+Bogdan-zh@users.noreply.github.com>
2018-01-26 18:35:47 +0200

    TwigCMS

--- TwigCMS/controllers/Catalog.php
+++ TwigCMS/controllers/Catalog.php
@@ -15,12 +15,7 @@ class Catalog extends Core
 
         $uri = parse_url($_SERVER['REQUEST_URI']);
         $parts = explode('/', $uri['path']);
-
-        // foreach($all_categories as $category) {
-        //     if($category['url'] == $parts[2]) {
-        //         $catalog = $categories->getCategory($category['id'], 'id');  
-        //     }
-        // }
+        
         if (isset($parts[2])) {
             $catalog = $categories->getCategory($parts[2], 'url');
         }
